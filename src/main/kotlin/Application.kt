ee78288ff35ebc9839db8a8e605762c1d988916b
Lars Schumann <lars.schumann@measuresforjustice.org>
2025-04-22 14:44:36 -0400

    initial example commit

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,11 @@
+package io.mfj
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.jetty.jakarta.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+}
